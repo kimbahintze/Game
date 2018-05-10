d4368e8ac0f6f941cf9d90cac9dc35c99ce645db
Isaac <isaacjgonzalez25@gmail.com>
2018-05-10 10:51:03 -0600

    Made Changes

---
+++ Game/Change.swift
@@ -0,0 +1,9 @@
+//
+//  Change.swift
+//  Game
+//
+//  Created by Isaac Gonzalez on 5/10/18.
+//  Copyright © 2018 Kim Lundquist. All rights reserved.
+//
+
+import Foundation
--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -12,11 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // Test develop change
-        // Isaac Added a comment
-        // Make another Change
-        // One more change
+        
     }
 
 
