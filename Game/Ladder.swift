94669dbee2bf7aed5fd0aa163f69cfb3fabed7d3
Seth <sethdanner@gmail.com>
2018-05-10 14:26:13 -0600

    adds Ladder, Point, and Snake

---
+++ Game/Ladder.swift
@@ -0,0 +1,21 @@
+//
+//  Ladder.swift
+//  Game
+//
+//  Created by Seth Danner on 5/10/18.
+//  Copyright © 2018 Kim Lundquist. All rights reserved.
+//
+
+import Foundation
+
+class Ladder {
+    
+    var positionTop: Point
+    var posiitionBottom: Point
+    // needed only for player?:    let range: Int = 1
+    
+    init(x: Int, y: Int) {
+        self.positionTop = Point(x: x, y: y)
+        self.posiitionBottom = Point(x: x, y: y)
+    }
+}
